e6278ece50774d85a3f75d4cda72a9a10afece89
Hnin-Nu-Hlaing <sakurayk2002@gmail.com>
2024-05-04 14:13:41 +0630

    js for looping

---
+++ script.js
@@ -0,0 +1,6 @@
+var students = [ "John" , "Justin" , "Kelvin" , "Rose" , "Sky"];
+console.log (students.length);
+
+for(var i = 0; i <  students.length; i++) {
+    console.log ("Great to see you " + students[i] + "!");
+};
